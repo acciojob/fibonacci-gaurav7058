2116cb8ea792f38cbab2ace5a1f389ae5e14737b
gaurav7058 <146345401+gaurav7058@users.noreply.github.com>
2024-07-15 15:32:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,20 @@
 function fibonacci(num) {
 // your code here
-	let n1 = 0, n2 = 1, nextTerm;
-for (let i = 1; i <= num; i++) {
-    nextTerm = n1 + n2;
-    n1 = n2;
-    n2 = nextTerm;
-}
-    return n1
+	 if (num <= 0) return 0;
+    if (num === 1) return 0;
+    if (num === 2) return 1;
+    
+    let a = 0;
+    let b = 1;
+    let fib = 1;
+
+    for (let i = 3; i <= num; i++) {
+        fib = a + b;
+        a = b;
+        b = fib;
+    }
+    
+    return fib;
 
 }
 
